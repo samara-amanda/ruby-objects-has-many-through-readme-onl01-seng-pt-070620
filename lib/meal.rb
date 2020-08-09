7ac694c8a8c6b00a5d4a26315d206f556f82e4b7
Samara Grant <samara.amanda01@gmail.com>
2020-08-09 03:02:55 +0000

    Done.

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,20 @@
 class Meal
+  attr_accessor :waiter, :customer, :total, :tip
+
+  def initialize(waiter, customer, total, tip)
+    @waiter = waiter
+    @customer = customer
+    @total = total
+    @tip = tip
+    save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 
 end
